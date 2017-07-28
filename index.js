63d09adad6c5e8ed1cbe652d9bd2199f333b5458
eacevedof <eacevedof@yahoo.es>
2017-07-28 14:47:53 +0200

    me da error shchema not defined

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 //index.js 1.0.1
+//https://www.npmjs.com/package/graphql
 require('babel-core/register')
 require('babel-polyfill')
 const oGraphSchema = require('./my_modules/graphql_schema')
 
 console.log("oGraphSchema",oGraphSchema)
+
+var query = '{ hello }';
+ 
+oGraphSchema(schema, query).then(result => {
+ 
+  // Prints 
+  // { 
+  //   data: { hello: "world" } 
+  // } 
+  console.log(result);
+ 
+});
