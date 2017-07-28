a328feb0e03a7874e0bdaf049f17e2f6d60041dd
eacevedof <eacevedof@yahoo.es>
2017-07-28 14:34:14 +0200

    instalo babel-core,polyfill,-preset-es2015 con yarn

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 //index.js 1.0.0 da error
 //SyntaxError: Unexpected token import
-import {
-  graphql,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString
-} from 'graphql';
- 
-var schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve() {
-          return 'world';
-        }
-      }
-    }
-  })
-});
+require('babel-core/register')
+require('babel-polyfill')
+require('./app')
